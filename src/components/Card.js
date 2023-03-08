23424292566ad3780e80c90644f63fc22edc441c
Jroutly3 <jroutly3@gatech.edu>
2023-03-07 22:35:39 -0500

    Created a PersonnelInfo screen

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
+import PersonnelInfo from "../pages/PersonnelInfo";
 import './Card.css'
 
 function Card({person}) {
+    const [sortField, setsortField] = useState(false);
+    function passPersonnel() {
+        console.log(person.DODID)
+        if (sortField === false) {
+            setsortField(true);
+          } else {
+            setsortField(false);
+          }
+    }
+    
     return(
-        <div className="tc bg-light-green dib br3 pa1 ma2 grow bw2 shadow-5 fl w-100">
-            <div className="card">
+        <div className="tc bg-light-green dib br3 pa1 ma2 grow bw2 shadow-5 fl w-100" >
+            <div className="card" onClick={passPersonnel}  person={person}>
                 <p>{person.Rank}</p>
                 <p>{person.name}</p>
                 <p>{person.unit}</p>
@@ -17,5 +28,6 @@ function Card({person}) {
         </div>
     )
 }
+//event =>  window.location.href='/PersonnelInfo'
 
 export default Card;
